3e642e8248eb7d31a848a56f12eb7c967905b2dd
gsladeira02 <gsladeira02@gmail.com>
2026-06-14 10:12:55 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-{
-  "name": "O Feirante",
-  "short_name": "Feirante",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#f7f3ea",
-  "theme_color": "#166534",
-  "icons": []
-}
